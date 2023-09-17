4f0bf9bfd19f9633508d6fe9f17f2ee95a641710
Kevinsaez <kevinsaez10@gmail.com>
2023-09-17 19:44:41 -0300

    navegacion

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import Home from './pages/home/Home';
-
+// import Home from './pages/home/Home'
+import Nav from './components/Navegacion/Navegacion'
 export default function App() {
+
   return (
-    <div>
-    <Home/>
+    <div >
+      <Nav/>
+      {/* <Home/> */}
     </div>
   );
 }
