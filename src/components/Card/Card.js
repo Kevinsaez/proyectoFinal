9c951e9c5d4a46053001de213a8fdcb4dbb9edad
Kevinsaez <kevinsaez10@gmail.com>
2023-09-19 19:45:21 -0300

    infocard mostar-ocultar

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,15 +1,16 @@
 import './Card.css';
 import InfoCard from '../InfoCard/infoCard.js';
+import { useState } from 'react';
 
 export default function Card({data}) {
-console.log(data)
+    const [mostrar,setMostrar]=useState(false);
     return(
         <div className="card bg-success border mb-2 p-2 d-flex flex-row gap-5">
             <div>
                 <img src={data.image} alt="imagen-personaje..."/>
                 <h3>{data.name}</h3>
-                <button>Know More..</button>
-                <InfoCard status={data.status} especie={data.species} genero={data.gender} origen={data.origin.name}/>
+                <button className={mostrar?'d-none':'d-block'} onClick={()=>setMostrar(true)}>Know More..</button>
+                {mostrar===true? <InfoCard status={data.status} especie={data.species} genero={data.gender} origen={data.origin.name} setMostrar={setMostrar}/>:''}
             </div>
         </div>
     )
