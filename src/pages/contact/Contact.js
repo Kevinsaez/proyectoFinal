936dcfb86f16e4bd564c98ddbc10c82cd676e927
Kevinsaez <kevinsaez10@gmail.com>
2023-10-02 02:02:03 -0300

    estilos-formulario-contacto

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,3 +1,4 @@
+import './Contact.css'
 import Nav from "../../components/Navegacion/Navegacion"
 import {Fragment} from 'react';
 
@@ -7,22 +8,22 @@ export default function contact(){
 return(
     <Fragment>
         <Nav itemMenu={'Characters'}/>
-        <main className='w-100 vh-100 d-flex align-items-center justify-content-center'>
-            <section className="bg-warning w-75 p-5 m-5">
-                <h2>Contact</h2>
-                <p>Leave us your information so we can contact you</p>
-                <form className="row g-3" action="" method="post">
+        <main className='container-fluid'>
+            <section className="d-flex flex-column seccion-contacto justify-content-center align-items-center m-auto mt-5 w-75 h-75">
+                <h2 className='py-5 fuente-color'>Contact</h2>
+                <p className='fuente-color fs-3'>Leave us your information so we can contact you</p>
+                <form className="row g-3 p-4 fuente-color" action="" method="post">
                     <div className="col-md-6">
                         <label htmlFor="name" className="form-label">Name</label>
-                        <input type="text" className="form-control" id="name"/>
+                        <input type="text" className="form-control p-2" id="name"/>
                     </div>
                     <div className="col-md-6">
                         <label htmlFor="email" className="form-label">Email</label>
-                        <input type="email" className="form-control" id="email"/>
+                        <input type="email" className="form-control p-2" id="email"/>
                     </div>
                     <div className="col-md-12">
                         <label htmlFor="message" className="form-label">Message</label>
-                        <textarea id="message" name="message" className="w-100"/>
+                        <textarea id="message" name="message" className="w-100 p-4"/>
                     </div>
                     <div className="col-12">
                         <input type="submit" className="btn btn-primary" value='Send'/>
